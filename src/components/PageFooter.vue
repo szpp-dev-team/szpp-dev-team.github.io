989db4b251da414dcd63b391b88937a90a1a2964
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-13 17:48:08 +0900

    feat: PageFooter を実装

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="page-footer">
+    <small class="copyright">&copy; 2022 SZPP</small>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.page-footer {
+  height: 80px;
+  background-color: #464646;
+  color: #fff;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > .copyright {
+    display: block;
+    text-align: center;
+  }
+}
+</style>
