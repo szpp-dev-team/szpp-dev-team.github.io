ee3355d24ad7fac0a1ff45939ca81c1bd8f15e69
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-18 21:56:32 +0900

    refactor: ヘッダとフッタの使用を layouts/default.vue へ移動

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import PageHeader from '../components/PageHeader.vue';
+import PageFooter from '../components/PageFooter.vue';
+
+</script>
+
 <template>
-  <main>
+  <PageHeader />
+  <main class="main">
     <router-view />
   </main>
+  <PageFooter />
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import PageHeader from "@/components/PageHeader.vue";
-import PageFooter from "@/components/PageFooter.vue";
 import TheHero from "@/components/TheHero.vue";
 import TheTopPageAbout from "@/components/TheTopPageAbout.vue";
 import TheTopPageInfos from "@/components/TheTopPageInfos.vue";
@@ -12,15 +10,10 @@ useHead({
 </script>
 
 <template>
-  <PageHeader />
-  <main>
-    <TheHero />
-    <TheTopPageAbout />
-    <TheTopPageInfos />
-  </main>
-  <PageFooter />
+  <TheHero />
+  <TheTopPageAbout />
+  <TheTopPageInfos />
 </template>
 
 <style scoped lang="scss">
-
 </style>
