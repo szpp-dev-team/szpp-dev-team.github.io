4a4b8949b7aeba09eb179634bc2bdf85cadc7789
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-19 02:47:41 +0900

    feat: 左右中央寄せにするユーティリティコンポーネント Center を追加

---
+++ src/components/Center.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="centering-container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.centering-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+}
+</style>
