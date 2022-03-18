5a8863e9c7285b2510d467d2cafb4e98fa3657f7
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-18 21:30:48 +0900

    fix: main.ts の app.use(head) はナシでも良さそう (むしろ warn が出る) なので削除

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,13 @@ import { ViteSSG } from 'vite-ssg';
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
-import { createHead } from '@vueuse/head';
 
 import './styles/common.scss'
 
 const routes = setupLayouts(generatedRoutes)
-const head = createHead();
 
 export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
-  ({ app }) => {
-    app.use(head);
-  },
+  () => { },
 )
