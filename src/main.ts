34a10bd57c38f7290af558e116df278b84eeb8f2
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-19 02:49:13 +0900

    feat: Center と LastUpdatedAt は import なしで使えるように (マークダウンで使えるように)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ViteSSG } from 'vite-ssg';
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
+import Center from '@/components/Center.vue';
+import LastUpdatedAt from '@/components/LastUpdatedAt.vue';
 
 import './styles/common.scss'
 
@@ -10,5 +12,8 @@ const routes = setupLayouts(generatedRoutes)
 export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
-  () => { },
+  ({ app }) => {
+    app.component('Center', Center);
+    app.component('LastUpdatedAt', LastUpdatedAt);
+  },
 )
