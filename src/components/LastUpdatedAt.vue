004c09094b407255d5b1cadaeddf94717f8da4a8
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-19 02:48:34 +0900

    feat: 最終更新日を表わすコンポーネントを追加

---
+++ src/components/LastUpdatedAt.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">import { computed } from 'vue';
+
+const props = defineProps<{
+  y: string | number,
+  m: string | number,
+  d: string | number,
+}>();
+
+const year = computed(() => String(props.y).padStart(4, '0'));
+const month = computed(() => String(props.m).padStart(2, '0'));
+const date = computed(() => String(props.d).padStart(2, '0'));
+
+const machineReadableDate = computed(() => `${year.value}-${month.value}-${date.value}`)
+const humanReadableDate = machineReadableDate;
+</script>
+
+<template>
+  <span class="last-updated">最終更新日：<time :datetime="machineReadableDate">{{ humanReadableDate }}</time></span>
+</template>
+
+<style scoped lang="scss">
+.last-updated {
+  color: var(--fg-secondary);
+}
+</style>
