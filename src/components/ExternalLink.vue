3d183d2f33702d52f670b8347b34646eff87e49b
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-20 01:28:18 +0900

    add: ExternalLink コンポーネントを追加

---
+++ src/components/ExternalLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  href: string,
+}>();
+</script>
+
+<template>
+  <a :href="href" target="_blank" rel="noreferrer noopener">
+    <slot />
+  </a>
+</template>
