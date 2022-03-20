752ce433efa919b65f877671ed58769af2977982
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-20 19:09:09 +0900

    feat: イベント一覧を実装

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -4,9 +4,29 @@ meta:
 </route>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import pages from '~pages';
+import EventList from '@/components/EventList.vue';
+
+const currentPath = useRoute().path;
+const events = pages.filter(p => p.path !== currentPath && p.path.startsWith(currentPath));
 </script>
 
 <template>
-  <pre><code>{{ pages }}</code></pre>
+  <section class="container">
+    <h1 class="top-header">イベント一覧</h1>
+    <p class="page-description">イベントの告知や事後報告の一覧です。</p>
+    <EventList :routes="events" />
+  </section>
 </template>
+
+<style scoped lang="scss">
+.top-header {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+.page-description {
+  margin: 1rem 0 2rem;
+}
+</style>
