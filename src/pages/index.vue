c161399a574c33ebed3322dd7236e8feff820f5e
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2022-03-20 23:30:02 +0900

    feat: トップページを最低限公開可能な見た目へ

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import TheHero from "@/components/TheHero.vue";
-import TheTopPageAbout from "@/components/TheTopPageAbout.vue";
-import TheTopPageInfos from "@/components/TheTopPageInfos.vue";
 import { useHead } from "@vueuse/head";
+import EventList from "@/components/EventList.vue";
+import PageSet from '@/modules/PageSet';
+import Center from "@/components/Center.vue";
 
 useHead({
   title: 'SZPP - 静岡大学プログラミングサークル'
@@ -10,10 +11,66 @@ useHead({
 </script>
 
 <template>
-  <TheHero />
-  <TheTopPageAbout />
-  <TheTopPageInfos />
+  <div class="top-page">
+    <TheHero />
+    <section class="about-szpp container">
+      <h2 class="top-page__section-header --border-bottom">SZPPとは</h2>
+      <p class="about-szpp__description">
+        SZPP はプログラミングを楽しむことを目的としたサークルです。
+        プログラミングに興味のある人なら誰でも歓迎です！
+        普段は競技プログラミングや勉強会、親睦を深めるための Among Us などをしています。
+      </p>
+      <Center>
+        <RouterLink class="detail-button" to="/about">もっと詳しく</RouterLink>
+      </Center>
+    </section>
+    <section class="newest-events container">
+      <h2 class="top-page__section-header">最新イベント</h2>
+      <EventList class="newest-events__event-list" :routes="PageSet.events().slice(0, 3)" />
+      <Center>
+        <RouterLink class="detail-button" to="/events">全てのイベントを見る</RouterLink>
+      </Center>
+    </section>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.top-page {
+  &__section-header {
+    color: var(--c-primary-main);
+    text-align: center;
+    margin: 0.5rem auto;
+    font-size: 2rem;
+
+    &.--border-bottom {
+      border-bottom: 1px solid var(--c-primary-main);
+    }
+  }
+}
+.about-szpp {
+  margin-top: 5rem;
+
+  &__description {
+    margin: 1rem auto;
+  }
+}
+.newest-events {
+  margin: 6rem auto 0;
+
+  &__event-list {
+    margin-bottom: 2rem;
+  }
+}
+.detail-button {
+  padding: 0.75rem 1.25rem;
+  font-size: 1.25rem;
+  background-color: var(--c-primary-main);
+  color: #fff;
+  border-radius: 8px;
+  font-weight: bold;
+
+  &:hover {
+    background-color: var(--c-primary-deep);
+  }
+}
 </style>
