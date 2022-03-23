bfd1d38127c33c53d57036453da202475ea5b032
arumakan1727 <arumakan1727@users.noreply.github.com>
2022-03-23 05:34:44 +0000

    deploy: 30a53ccb4953bd33ae36fcaf2666f6d5713024d0

---
+++ assets/article.b52dd4a3.js
@@ -0,0 +1,1 @@
+import{_ as t,a as _,c as o}from"./app.00e087d1.js";import{d as r,r as s,o as c,c as n,a as e,e as i}from"./vendor.7fddc372.js";const l={class:"main"},p=r({setup(d){return(u,v)=>{const a=s("router-view");return c(),n("div",null,[e(_),i("main",l,[e(a)]),e(o)])}}});var x=t(p,[["__scopeId","data-v-50be2cd7"]]);export{x as default};
