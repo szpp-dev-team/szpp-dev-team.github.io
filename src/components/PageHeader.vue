28fb8f7a6a7b634ad79bdca219e8826dec7431d7
kaji <kajiharalivi@icloud.com>
2022-03-23 16:46:22 +0900

    feat: pages/index.vueに製作物紹介を作成。component/PageHeader.vueに製作物欄を作成

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -34,6 +34,9 @@ const handleLinkClick = () => { sideMenuVisible.value = false; };
           <li class="nav__link-list__item">
             <RouterLink class="nav__link-list__link" to="/events" @click="handleLinkClick">イベント</RouterLink>
           </li>
+          <li clas="nav__link-list__item">
+            <RouterLink class="nav__link-list__link" to="/products" @click="handleLinkClick">製作物</RouterLink>
+          </li>
           <li class="nav__link-list__item">
             <RouterLink class="nav__link-list__link" to="/faq" @click="handleLinkClick">よくある質問</RouterLink>
           </li>
---
+++ src/pages/products/index.vue
@@ -0,0 +1,17 @@
+<route lang="yaml">
+meta:
+  title: 製作物一覧
+</route>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head';
+
+useHead({
+  meta: [
+    {
+      name: 'discription',
+      content: 'SZPPの製作物一覧'
+    }
+  ]
+});
+</script>
