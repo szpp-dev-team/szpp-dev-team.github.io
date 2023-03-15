2d1ec98a3889fd64a0258682b5fc0cc1cbd6d9e4
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-15 21:27:27 +0900

    fix: Markdownの内容をArticleコンポーネントへ

---
+++ src/components/Article.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+const props = defineProps<{
+  frontmatter: {
+    meta: object[],
+  },
+}>();
+
+console.log(props.frontmatter.meta);
+</script>
+
+<template>
+  <article>
+    <slot />
+  </article>
+</template>
+
+<style lang="scss">
+article {
+  background-color: #fff;
+  min-height: calc(100vh - var(--height-header) - var(--height-footer));
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 5rem 5rem;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, .2);
+
+  @media screen and (max-width: 768px) {
+    & {
+      padding: 5rem 2rem;
+    }
+  }
+
+  h1 {
+    color: #404040;
+    font-size: 3rem;
+    margin-bottom: 2rem;
+    font-weight: 500;
+  }
+
+  h2 {
+    color: var(--c-primary-dark);
+    font-size: 2.25rem;
+    width: 100%;
+    border-bottom: 1px solid var(--c-primary-dark);
+    margin: 4rem 0 1rem;
+    font-weight: 500;
+  }
+
+  h3 {
+    color: var(--c-primary-dark);
+    font-size: 1.75rem;
+    border-bottom: 2px dotted #b0b0c0;
+    margin: 2.75rem 0 0.5rem;
+    font-weight: 500;
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    margin: 2.75rem 0 0.5rem;
+    padding-left: 0.5rem;
+    border-left: 4px solid var(--c-primary-dark);
+    color: var(--c-primary-dark);
+  }
+
+  h2+h3,
+  h3+h4 {
+    margin-top: 1.5rem;
+  }
+
+  a {
+    color: #2078e5;
+    text-decoration: underline;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  hr {
+    border-top: 2px solid #ccc;
+    margin: 1.5rem auto;
+    width: 70%;
+    min-width: 200px;
+  }
+
+
+  img,
+  media,
+  figure {
+    margin: 2rem auto;
+  }
+
+  figure>img {
+    margin: .5rem auto;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import generatedRoutes from 'virtual:generated-pages';
 import { setupLayouts } from 'virtual:generated-layouts';
 import App from './App.vue';
 import Center from '@/components/Center.vue';
+import Article from '@/components/Article.vue';
 import LastUpdatedAt from '@/components/LastUpdatedAt.vue';
 import VueGtag from 'vue-gtag';
 
@@ -15,6 +16,7 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   ({ app }) => {
     app.component('Center', Center);
+    app.component('Article', Article);
     app.component('LastUpdatedAt', LastUpdatedAt);
     if (import.meta.env.MODE === 'production' && import.meta.env.VITE_GA_MEASUREMENT_ID) {
       app.use(VueGtag, {
