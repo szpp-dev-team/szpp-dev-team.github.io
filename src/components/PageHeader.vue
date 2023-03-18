ae8a00044fa1439cdc1bc4fc6bc7d11cf066acf5
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-18 15:41:28 +0900

    refactor: PageHeaderの高さ分だけ余白をとる方法を、空のdivを使うのではなくbodyにmargin-topを設定する方式に変更

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -21,7 +21,11 @@ const handleBackdropClick = () => { sideMenuVisible.value = false; };
 </script>
 
 <template>
-  <div>
+  <header
+    class="header"
+    :class="headerVisible ? '' : 'invisible'"
+    v-bind="$attrs"
+  >
     <Teleport to="body">
       <div
         class="nav__backdrop"
@@ -29,55 +33,87 @@ const handleBackdropClick = () => { sideMenuVisible.value = false; };
         @click="handleBackdropClick"
       ></div>
     </Teleport>
-    <header class="header" :class="headerVisible ? '' : 'invisible'" v-bind="$attrs">
-      <nav class="nav">
-        <RouterLink class="logo" to="/">
-          <img class="logo__img" src="/szpp-logo-untransparent.jpeg" alt="SZPP's logo" />
-          <span>SZPP</span>
-        </RouterLink>
-        <ul class="nav__link-list" :class="sideMenuVisible ? '' : 'invisible'">
-          <li class="nav__link-list__item">
-            <RouterLink class="nav__link-list__link" to="/about" @click="handleLinkClick">SZPP について</RouterLink>
-          </li>
-          <li class="nav__link-list__item">
-            <RouterLink class="nav__link-list__link" to="/news" @click="handleLinkClick">お知らせ</RouterLink>
-          </li>
-          <li class="nav__link-list__item">
-            <RouterLink class="nav__link-list__link" to="/products" @click="handleLinkClick">製作物</RouterLink>
-          </li>
-          <li class="nav__link-list__item">
-            <RouterLink class="nav__link-list__link" to="/faq" @click="handleLinkClick">よくある質問</RouterLink>
-          </li>
-          <li class="nav__link-list__item">
-            <ExternalLink class="nav__link-list__link --icon" href="https://github.com/szpp-dev-team/">
-              <i class="fa-brands fa-github"></i>
-              <span>GitHub</span>
-            </ExternalLink>
-          </li>
-          <li class="nav__link-list__item">
-            <ExternalLink class="nav__link-list__link --icon" href="https://twitter.com/szpp_3776">
-              <i class="fa-brands fa-twitter"></i>
-              <span>Twitter</span>
-            </ExternalLink>
-          </li>
-        </ul>
-        <button
-          class="menu-toggle-button"
-          :class="sideMenuVisible ? 'active' : ''"
-          @click="handleMenuButtonClick"
-        >
-          <div class="menu-toggle-button__container">
-            <span class="menu-toggle-button__line1"></span>
-            <span class="menu-toggle-button__line2"></span>
-            <span class="menu-toggle-button__line3"></span>
-          </div>
-        </button>
-      </nav>
-    </header>
-    <div class="transparent-height-keep-header"></div>
-  </div>
+    <nav class="nav">
+      <RouterLink class="logo" to="/">
+        <img
+          class="logo__img"
+          src="/szpp-logo-untransparent.jpeg"
+          alt="SZPP's logo"
+        />
+        <span>SZPP</span>
+      </RouterLink>
+      <ul class="nav__link-list" :class="sideMenuVisible ? '' : 'invisible'">
+        <li class="nav__link-list__item">
+          <RouterLink
+            class="nav__link-list__link"
+            to="/about"
+            @click="handleLinkClick"
+          >SZPP について</RouterLink
+          >
+        </li>
+        <li class="nav__link-list__item">
+          <RouterLink
+            class="nav__link-list__link"
+            to="/news"
+            @click="handleLinkClick"
+          >お知らせ</RouterLink
+          >
+        </li>
+        <li class="nav__link-list__item">
+          <RouterLink
+            class="nav__link-list__link"
+            to="/products"
+            @click="handleLinkClick"
+          >製作物</RouterLink
+          >
+        </li>
+        <li class="nav__link-list__item">
+          <RouterLink
+            class="nav__link-list__link"
+            to="/faq"
+            @click="handleLinkClick"
+          >よくある質問</RouterLink
+          >
+        </li>
+        <li class="nav__link-list__item">
+          <ExternalLink
+            class="nav__link-list__link --icon"
+            href="https://github.com/szpp-dev-team/"
+          >
+            <i class="fa-brands fa-github"></i>
+            <span>GitHub</span>
+          </ExternalLink>
+        </li>
+        <li class="nav__link-list__item">
+          <ExternalLink
+            class="nav__link-list__link --icon"
+            href="https://twitter.com/szpp_3776"
+          >
+            <i class="fa-brands fa-twitter"></i>
+            <span>Twitter</span>
+          </ExternalLink>
+        </li>
+      </ul>
+      <button
+        class="menu-toggle-button"
+        :class="sideMenuVisible ? 'active' : ''"
+        @click="handleMenuButtonClick"
+      >
+        <div class="menu-toggle-button__container">
+          <span class="menu-toggle-button__line1"></span>
+          <span class="menu-toggle-button__line2"></span>
+          <span class="menu-toggle-button__line3"></span>
+        </div>
+      </button>
+    </nav>
+  </header>
 </template>
 
+<style lang="scss">
+body {
+  margin-top: var(--height-header) !important;
+}
+</style>
 
 <style scoped lang="scss">
 .header {
