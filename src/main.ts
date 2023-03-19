e4ff19f70a345a1b3e2c3be3daae359af9cd0ec3
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-20 01:04:09 +0900

    fix: vue-routerにscrollBehaviorを定義

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,22 @@ const routes = setupLayouts(generatedRoutes);
 export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
-  ({ app }) => {
-    app.component('Center', Center);
-    app.component('Article', Article);
-    app.component('LastUpdatedAt', Date);
-    if (import.meta.env.MODE === 'production' && import.meta.env.VITE_GA_MEASUREMENT_ID) {
+  ({ app, router }) => {
+    app.component("Center", Center);
+    app.component("Article", Article);
+    app.component("LastUpdatedAt", Date);
+    if (
+      import.meta.env.MODE === "production" &&
+      import.meta.env.VITE_GA_MEASUREMENT_ID
+    ) {
       app.use(VueGtag, {
         config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID },
       });
     }
+    router.options.scrollBehavior = (to, _from, savedPosition) => {
+      if (savedPosition) return savedPosition;
+      if (to.hash) return { el: to.hash };
+      return { left: 0, top: 0, behavior: "auto" };
+    };
   }
 );
