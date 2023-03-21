0807e0af96722794f21dd2efc5286355a22844df
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-22 01:23:10 +0900

    chore: prettierを導入, eslintの設定を修正

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-airbnb-with-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+
+  rules: {
+    "vue/component-tags-order": [
+      "error",
+      {
+        order: ["script", "template", "style"],
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/typedef": [
+      "error",
+      {
+        variableDeclaration: false,
+        memberVariableDeclaration: true,
+        propertyDeclaration: true,
+        variableDeclarationIgnoreFunction: true,
+        arrayDestructuring: false,
+        objectDestructuring: false,
+        parameter: true,
+        arrowParameter: false,
+      },
+    ],
+  },
+};
