0f6b09a7412d3370ce671109b96e8c434cb252b7
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-22 12:03:37 +0900

    refactor: TheArticle.vueをorganisms/へ

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ViteSSG } from "vite-ssg";
 import generatedRoutes from "virtual:generated-pages";
 import { setupLayouts } from "virtual:generated-layouts";
-import TheArticle from "@/components/TheArticle.vue";
+import TheArticle from "@/components/organisms/TheArticle.vue";
 import DateText from "@/components/atoms/DateText.vue";
 import VueGtag from "vue-gtag";
 import App from "./App.vue";
