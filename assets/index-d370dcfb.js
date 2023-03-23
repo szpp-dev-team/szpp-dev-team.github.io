dd8bfcea5a4396a24bf570603bda23a260e1526a
arumakan1727 <arumakan1727@users.noreply.github.com>
2023-03-23 03:37:29 +0000

    deploy: d70295c948a53671b7594b3929fc0f328430d567

--- assets/index-d370dcfb.js
+++
@@ -1,1 +0,0 @@
-import{e as t,r as c,o as r,c as s,f as e}from"./app-3a54c6e8.js";const o={};function i(a,p){const n=c("article-index-tmpl");return r(),s(n)}typeof e=="function"&&e(o);const f=t(o,[["render",i],["__file","/home/runner/work/szpp-dev-team.github.io/szpp-dev-team.github.io/src/pages/news/index.vue"]]);export{f as default};
